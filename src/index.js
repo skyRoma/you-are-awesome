fb20a6292e8126ed13057f1de60fec8c2cde6fa2
skyRoma <roma.sluka.97@mail.ru>
2018-03-14 22:59:47 +0300

    solutiom 0.5

--- src/index.js
+++ src/index.js
@@ -1,9 +1,43 @@
-// DO WHATEVER YOU WANT HERE
+const createEnumerableProperty = (propertyName) => {
+    Object.defineProperty(Object.prototype, propertyName, {
+        configurable: true,
+        writable: true
+    });
+    return propertyName;
+};
 
-const createEnumerableProperty = () => {};
-const createNotEnumerableProperty = () => {};
-const createProtoMagicObject = () => {};
-const incrementor = () => {};
+const createNotEnumerableProperty = (propertyName) => {
+    Object.defineProperty(Object.prototype, propertyName, {
+        get: function() {
+            return Object.prototype.propertyName;
+        },
+
+        set: function(value) {
+            Object.prototype.propertyName = value;
+        }
+    });
+    return propertyName;
+};
+
+const createProtoMagicObject = () => {
+    function magicObj() {};
+    magicObj.__proto__ = magicObj.prototype;
+    return magicObj;
+};
+
+let currentSum = 0;
+const incrementor = () => {
+    currentSum++;
+
+    function f() {
+        currentSum++;
+        return f;
+    }
+    f.toString = function() {
+        return currentSum;
+    };
+    return f;
+};
 const asyncIncrementor = () => {};
 const createIncrementer = () => {};
 
@@ -12,7 +46,9 @@ const returnBackInSecond = () => {};
 const getDeepPropertiesCount = () => {};
 const createSerializedObject = () => {};
 const toBuffer = () => {};
-const sortByProto = () => {};
+const sortByProto = (array) => {
+    return array;
+};
 
 exports.createEnumerableProperty = createEnumerableProperty;
 exports.createNotEnumerableProperty = createNotEnumerableProperty;
